c1803a42cea41997bc37c73627ca611e9261992b
PavelProtchenko <protchenkopa@gmail.com>
2019-01-06 12:08:56 +0300

    add submenu

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const mainMenu = document.querySelectorAll('#main li');
 const registerWindow = document.querySelector('#register');
 const accountLink = document.querySelector('#account');
+const mainLink = document.querySelector('#m-menu');
 
 const mainMenuClick = () => {
-  if(accountLink) {
-    registerWindow.style.visibility='visible';
-  }else {
-    alert('You clicked me!');
-  }
+  mainMenu.forEach(el => {
+    if(el.id == accountLink.id) {
+      return registerWindow.style.visibility='visible';
+    }else if(el.id == mainLink.id) {
+      alert('Main Link');
+    }else{
+      alert('You clicked me!');
+    }
+  })
 };
 
 const mainMenuLink = mainMenu.forEach(link => {
