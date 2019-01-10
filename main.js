c404467a132f7c4b1a4cd71910406767ebb14a2c
PavelProtchenko <protchenkopa@gmail.com>
2019-01-11 02:05:54 +0300

    clean javascript code

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 window.onload = () => {
   const registerWindow = document.querySelector('#register');
+  const mainMenu = document.querySelector('#main').addEventListener('click', clickList);
+  const tabsHandler = document.querySelector('.tabs-header').addEventListener('click', fTabs);
 
-  const clickList = (event) => {
+  function clickList(event) {
     if(event.target.className == 'menu-item') {
-      let dataList = document.querySelectorAll('.menu-item');
+      let dataList = document.querySelector('.menu-item');
       for(let i = 0; i < dataList.length; i++) {
       if(dataList[i].getAttribute('data-list') == 'account') {
         return registerWindow.style.visibility = 'visible';
@@ -16,23 +18,19 @@ window.onload = () => {
     }
   };
 
-  const mainMenu = document.querySelector('#main').addEventListener('click', clickList);
-
-
-  document.querySelector('.tabs-header').addEventListener('click', fTabs);
 
   function fTabs(event) {
     if(event.target.className == 'tab-h') {
       // dataTab - tab number is needede to be displayed
       var dataTab = event.target.getAttribute('data-tab');
       // switch off active class
-      var tabH = document.getElementsByClassName('tab-h');
+      var tabH = document.querySelectorAll('.tab-h');
       for(var i = 0; i < tabH.length; i++) {
         tabH[i].classList.remove('active');
       }
       event.target.classList.add('active');
       // all tabs with content
-      var tabBody = document.getElementsByClassName('tab-b');
+      var tabBody = document.querySelectorAll('.tab-b');
       for(var i = 0; i < tabBody.length; i++) {
         if(dataTab == i) {
           tabBody[i].style.display = 'block';
